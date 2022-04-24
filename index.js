184a244bf055adc38e6e33d30b8638ad5fda1d35
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-04-24 13:25:38 +0300

    Create 3 Constructor Functions, Show Task and Reset Form Part1

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// I wish you good luck and happy coding 🥰🤠🥳🥳💯💯
+document.querySelector(".AddTaskBtn").addEventListener("click", (e) => {
+  // console.log(e.target);
+  const textTitle = document.querySelector("#newtaskID").value;
+  console.log(textTitle);
+});
