c22fb25b2061064cfc6bad099c0f08bc9287e3a1
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-04-29 16:18:35 +0300

    Show List of tasks from LocalStorage

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import Task from "./task.js";
 import UI from "./ui.js";
 
 const ui = new UI();
+ui.showAllTasks();
+
 document.querySelector(".AddTaskBtn").addEventListener("click", (e) => {
   const textTitle = document.querySelector("#newtaskID").value;
 
